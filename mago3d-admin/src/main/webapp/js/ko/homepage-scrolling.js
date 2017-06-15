709b4ba9a130e198d85aa91427fe0348e1900c30
임호준 <hjlim@gaia3d.com>
2017-06-15 16:34:31 +0900

    scrolling.js error 수정

--- mago3d-admin/src/main/webapp/js/ko/homepage-scrolling.js
+++ mago3d-admin/src/main/webapp/js/ko/homepage-scrolling.js
@@ -14,27 +14,3 @@
 			}
 		});
 	});
-
-
-
-<!-- 
-	$(document).ready(function(){
-		$(window).scroll(function(e){
-			var nav = $('.sub_page_menu');
-			var header = $('header');
-			
-			var windowScrollTop = $(window).scrollTop()
-			var navOffsetTop = nav.offset().top;
-			
-			var headerTop = header.offset().top;
-			var headerHeight = header.outerHeight();
-			var headerBottom = headerTop + headerHeight;
-			
-			if((navOffsetTop < windowScrollTop) || (headerBottom < windowScrollTop)){
-				nav.offset({top:windowScrollTop}).css({color:'white',border:'1px solid white'});
-			}else if(headerBottom > windowScrollTop){
-				nav.offset({top:headerBottom}).css({color:'black',border:'1px solid black'});
-			}
-		});
-	});
- -->
