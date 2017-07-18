d921647b6420ea8bc593d73868cc2bbc495afbe6
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2017-07-18 15:05:09 +0900

    임시 커밋

--- mago3d-admin/src/main/java/com/gaia3d/controller/CommonCodeController.java
+++ mago3d-admin/src/main/java/com/gaia3d/controller/CommonCodeController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gaia3d.config.CacheConfig;
--- mago3d-admin/src/test/java/com/gaia3d/PasswordTest.java
+++ mago3d-admin/src/test/java/com/gaia3d/PasswordTest.java
@@ -1,20 +1,20 @@
 package com.gaia3d;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class PasswordTest {
 
 	@Test
 	public void test() {
-		String salt = "aaa";
-		
+		String salt = BCrypt.gensalt();
 		ShaPasswordEncoder shaPasswordEncoder = new ShaPasswordEncoder(512);
 		shaPasswordEncoder.setIterations(1000);
-		String encriptPassword = shaPasswordEncoder.encodePassword("test", salt) ;
+		String encriptPassword = shaPasswordEncoder.encodePassword("test", salt);
+		System.out.println("salt = " + salt);
 		System.out.println(encriptPassword);
 	}
 
 }
+	
