e2d4f0eba69f4a15133ce5fe07a80827b0b08bcc
Hirofum Hayashi <pica.hayashi@gmail.com>
2018-02-12 22:06:41 +0900

    Translate strings
    Translate strings from korean to japanese.

--- mago3d-user/src/main/webapp/js/ja/message.js
+++ mago3d-user/src/main/webapp/js/ja/message.js
@@ -1,91 +1,91 @@
 var JS_MESSAGE = new Object();
 
-JS_MESSAGE["create"] = "생성 되었습니다.";
-JS_MESSAGE["insert"] = "등록 되었습니다.";
-JS_MESSAGE["update"] = "수정 되었습니다.";
-JS_MESSAGE["delete"] = "삭제 되었습니다.";
-JS_MESSAGE["success"] = "성공 하였습니다.";
-JS_MESSAGE["apply"] = "적용되었습니다.";
-JS_MESSAGE["upload"] = "업로딩 중입니다.";
-JS_MESSAGE["move.confirm"] = "이동 하시겠습니까?";
-JS_MESSAGE["delete.confirm"] = "삭제 하시겠습니까?";
-JS_MESSAGE["check.value.required"] = "선택된 항목이 없습니다.";
+JS_MESSAGE["create"] = "作成されました。";
+JS_MESSAGE["insert"] = "登録されました。";
+JS_MESSAGE["update"] = "修正されました。";
+JS_MESSAGE["delete"] = "削除されました。";
+JS_MESSAGE["success"] = "成功しました。";
+JS_MESSAGE["apply"] = "適用されました。";
+JS_MESSAGE["upload"] = "アップロード中です。";
+JS_MESSAGE["move.confirm"] = "移動しますか？";
+JS_MESSAGE["delete.confirm"] = "削除しますか？";
+JS_MESSAGE["check.value.required"] = "選択された項目がありません。";
 
-// 공통
-JS_MESSAGE["user.session.empty"] = "로그인 후 사용 가능한 서비스 입니다.";
-JS_MESSAGE["db.exception"] = "데이터 베이스 장애가 발생하였습니다. 잠시 후 다시 이용하여 주시기 바랍니다.";
-JS_MESSAGE["ajax.error.message"] = "잠시 후 이용해 주시기 바랍니다. 장시간 같은 현상이 반복될 경우 관리자에게 문의하여 주십시오.";
-JS_MESSAGE["button.dobule.click"] = "진행 중입니다.";
-JS_MESSAGE["cache.reloaded"] = "캐시를 갱신 하였습니다.";
-JS_MESSAGE["usersession.grant.invalid"] = "사용 권한이 유효하지 않습니다.";
+//共通
+JS_MESSAGE["user.session.empty"] = "ログイン後に使用可能なサービスです。";
+JS_MESSAGE["db.exception"] = "データベースに障害が発生しました。しばらくしてから再度ご利用ください。";
+JS_MESSAGE["ajax.error.message"] = "しばらくしてご利用下さい。長時間同じ現象が繰り返される場合は、管理者に問い合わせてください。";
+JS_MESSAGE["button.dobule.click"] = "進行中です。";
+JS_MESSAGE["cache.reloaded"] = "キャッシュを更新しました。";
+JS_MESSAGE["usersession.grant.invalid"] = "アクセス許可が有効ではありません。";
 
-JS_MESSAGE["login.password.decrypt.exception"] = "로그인 비밀번호 처리 과정에서 오류가 발생하였습니다.";
+JS_MESSAGE["login.password.decrypt.exception"] = "ログインパスワード処理中にエラーが発生しました。";
 
-//사용자
-JS_MESSAGE["user.id.empty"] = "아이디를 입력하여 주십시오.";
-JS_MESSAGE["user.id.min_length.invalid"] = "사용자 아이디 최소 길이가 올바르지 않습니다.";
-JS_MESSAGE["password.empty"] = "비밀번호를 입력하여 주십시오.";
-JS_MESSAGE["password.correct.empty"] = "비밀번호 확인을 입력하여 주십시오.";
-JS_MESSAGE["user.name.empty"] = "이름을 입력하여 주십시오.";
-JS_MESSAGE["user.input.invalid"] = "필수 입력값이 유효하지 않습니다.";
-JS_MESSAGE["user.id.duplication"] = "사용중인 아이디 입니다. 다른 아이디를 선택해 주십시오.";
-JS_MESSAGE["user.password.invalid"] = "입력한 패스워드가 설정된 패스워드 정책에 부적합 합니다.";
-JS_MESSAGE["user.password.digit.invalid"] = "입력한 패스워드가 설정된 패스워드 정책에(숫자 개수) 부적합 합니다.";
-JS_MESSAGE["user.password.upper.invalid"] = "입력한 패스워드가 설정된 패스워드 정책에(영문 대문자 개수) 부적합 합니다.";
-JS_MESSAGE["user.password.lower.invalid"] = "입력한 패스워드가 설정된 패스워드 정책에(영문 소문자 개수) 부적합 합니다.";
-JS_MESSAGE["user.password.special.invalid"] = "입력한 패스워드가 설정된 패스워드 정책에(특수 문자 개수) 부적합 합니다.";
-JS_MESSAGE["user.password.continuous.char.invalid"] = "연속 문자 제한 개수가 패스워드 정책에 부적합 합니다.";
-JS_MESSAGE["user.password.exception.char.message1"] = "관리자가 설정한 특수문자 ";
-JS_MESSAGE["user.password.exception.char.message2"] = " 는 비밀번호로 사용 하실 수 없습니다.";
-JS_MESSAGE["user.password.exception"] = "패스워드 등록 과정 중 오류가 발생하였습니다.";
-JS_MESSAGE["user.session.notexist"] = "세션 정보가 존재하지 않습니다.";
-JS_MESSAGE["user.session.closed"] = "세션 종료 처리 하였습니다.";
-JS_MESSAGE["user.session.close"] = "선택하신 사용자의 세션을 종료 하시겠습니까?";
-JS_MESSAGE["user.id.enable"] = "사용 가능한 아이디 입니다.";
-JS_MESSAGE["user.insert"] = "사용자를 등록 하였습니다.";
-JS_MESSAGE["user.info.update"] = "사용자 정보를 수정 하였습니다.";
-JS_MESSAGE["user.id.notexist"] = "아이디가 존재하지 않습니다.";
+//ユーザー
+JS_MESSAGE["user.id.empty"] = "ユーザ名を入力してください。";
+JS_MESSAGE["user.id.min_length.invalid"] = "ユーザー名が短すぎます。";
+JS_MESSAGE["password.empty"] = "パスワードを入力してください。";
+JS_MESSAGE["password.correct.empty"] = "パスワードの確認を入力してください。";
+JS_MESSAGE["user.name.empty"] = "名前を入力してください。";
+JS_MESSAGE["user.input.invalid"] = "必須入力値が有効ではありません。";
+JS_MESSAGE["user.id.duplication"] = "使用中のIDです。他のIDを選択してください。";
+JS_MESSAGE["user.password.invalid"] = "入力したパスワードが設定されたパスワードポリシーに不適合です。";
+JS_MESSAGE["user.password.digit.invalid"] = "入力したパスワードが設定されたパスワードポリシーの（数字の数）に不適合です。";
+JS_MESSAGE["user.password.upper.invalid"] = "入力したパスワードが設定されたパスワードポリシーの（英語の大文字数）に不適合です。";
+JS_MESSAGE["user.password.lower.invalid"] = "入力したパスワードが設定されたパスワードポリシーの（英語の小文字数）に不適合です。";
+JS_MESSAGE["user.password.special.invalid"] = "入力したパスワードが設定されたパスワードポリシーの（特殊文字の数）に不適合です。";
+JS_MESSAGE["user.password.continuous.char.invalid"] = "継続文字制限数がパスワードポリシーに不適合です。";
+JS_MESSAGE["user.password.exception.char.message1"] = "管理者が設定した特殊文字";
+JS_MESSAGE["user.password.exception.char.message2"] = "は、パスワードとして使用することができません。";
+JS_MESSAGE["user.password.exception"] = "パスワードの登録処理中にエラーが発生しました。";
+JS_MESSAGE["user.session.notexist"] = "セッション情報が存在しません。";
+JS_MESSAGE["user.session.closed"] = "セッション終了処理しました。";
+JS_MESSAGE["user.session.close"] = "選択したユーザーのセッションを終了しますか？";
+JS_MESSAGE["user.id.enable"] = "使用可能なIDです。";
+JS_MESSAGE["user.insert"] = "ユーザーを登録しました。";
+JS_MESSAGE["user.info.update"] = "ユーザー情報を修正しました。";
+JS_MESSAGE["user.id.notexist"] = "ユーザ名が存在しません。";
 
 
-//운영정책
-JS_MESSAGE["policy.server.datetime"] = "서버 시간이 재설정되었습니다."
-JS_MESSAGE["policy.user.update"] = "사용자 정책을 수정 하였습니다.";
-JS_MESSAGE["policy.password.update"] = "패스워드 정책을 수정 하였습니다.";
-JS_MESSAGE["policy.geo.update"] = "공간 정보를 수정 하였습니다.";
-JS_MESSAGE["policy.geoserver.update"] = "GeoServer를 수정 하였습니다.";
-JS_MESSAGE["policy.geocallback.update"] = "CallBack 함수를 수정 하였습니다.";
-JS_MESSAGE["policy.notice.update"] = "알림 정책을 수정 하였습니다.";
-JS_MESSAGE["policy.security.update"] = "보안 정책을 수정 하였습니다.";
-JS_MESSAGE["policy.content.update"] = "컨텐트 정책을 수정 하였습니다.";
-JS_MESSAGE["policy.site.update"] = "사이트 정보를 수정 하였습니다.";
-JS_MESSAGE["policy.os.update"] = "OS 설정 정보를 수정 하였습니다.";
-JS_MESSAGE["policy.backoffice.update"] = "Back Office 정보를 수정 하였습니다.";
-JS_MESSAGE["policy.solution.update"] = "제품 정보를 수정 하였습니다.";
-JS_MESSAGE["policy.content.invalid"] = "필수 입력값이 유효하지 않습니다.";
+//運営ポリシー
+JS_MESSAGE["policy.server.datetime"] = "サーバーの時間がリセットされました。"
+JS_MESSAGE["policy.user.update"] = "ユーザーポリシーを変更しました。";
+JS_MESSAGE["policy.password.update"] = "パスワードポリシーを変更しました。";
+JS_MESSAGE["policy.geo.update"] = "空間情報を修正しました。";
+JS_MESSAGE["policy.geoserver.update"] = "GeoServerを修正しました。";
+JS_MESSAGE["policy.geocallback.update"] = "CallBack関数を修正しました。";
+JS_MESSAGE["policy.notice.update"] = "通知ポリシーを修正しました。";
+JS_MESSAGE["policy.security.update"] = "セキュリティポリシーを変更しました。";
+JS_MESSAGE["policy.content.update"] = "コンテンツポリシーを変更しました。";
+JS_MESSAGE["policy.site.update"] = "サイトの情報を修正しました。";
+JS_MESSAGE["policy.os.update"] = "OSの設定情報を変更しました。";
+JS_MESSAGE["policy.backoffice.update"] = "Back Officeの情報を修正しました。";
+JS_MESSAGE["policy.solution.update"] = "製品情報を修正しました。";
+JS_MESSAGE["policy.content.invalid"] = "必須入力値が有効ではありません。";
 
-// 데이터
-JS_MESSAGE["data.key.empty"] = "Key를 입력하여 주십시오.";
-JS_MESSAGE["data.key.duplication_value.check"] = "Key 중복확인을 해주십시오."
-JS_MESSAGE["data.key.duplication_value.already"] = "사용중인 Key 입니다. 다른 Key를 선택해 주십시오.";
-JS_MESSAGE["data.project.id.empty"] = "프로젝트을 선택하여 주십시오.";
-JS_MESSAGE["data.latitude.empty"] = "위도를 입력하여 주십시오.";
-JS_MESSAGE["data.longitude.empty"] = "경도를  입력하여 주십시오.";
-JS_MESSAGE["data.height.empty"] = "높이를  입력하여 주십시오.";
-JS_MESSAGE["data.heading.empty"] = "Heading을  입력하여 주십시오.";
-JS_MESSAGE["data.pitch.empty"] = "Pitch를 입력하여 주십시오.";
-JS_MESSAGE["data.roll.empty"] = "Roll을 입력하여 주십시오.";
-JS_MESSAGE["data.key.duplication"] = "사용중인 아이디 입니다. 다른 아이디를 선택해 주십시오.";
-JS_MESSAGE["data.key.enable"] = "사용 가능한 Key 입니다.";
-JS_MESSAGE["data.insert"] = "데이터를 등록 하였습니다.";
-JS_MESSAGE["user.info.update"] = "사용자 정보를 수정 하였습니다.";
+//データ
+JS_MESSAGE["data.key.empty"] = "Keyを入力してください。";
+JS_MESSAGE["data.key.duplication_value.check"] = "Key重複確認をしてください。"
+JS_MESSAGE["data.key.duplication_value.already"] = "使用中のKeyです。他のKeyを選択してください。";
+JS_MESSAGE["data.project.id.empty"] = "プロジェクトを選択してください。";
+JS_MESSAGE["data.latitude.empty"] = "緯度を入力してください。";
+JS_MESSAGE["data.longitude.empty"] = "経度を入力してください。";
+JS_MESSAGE["data.height.empty"] = "高さを入力してください。";
+JS_MESSAGE["data.heading.empty"] = "Headingを入力してください。";
+JS_MESSAGE["data.pitch.empty"] = "Pitchを入力してください。";
+JS_MESSAGE["data.roll.empty"] = "Rollを入力してください。";
+JS_MESSAGE["data.key.duplication"] = "使用中のIDです。他のIDを選択してください。";
+JS_MESSAGE["data.key.enable"] = "使用可能なKeyです。";
+JS_MESSAGE["data.insert"] = "データを登録しました。";
+JS_MESSAGE["user.info.update"] = "ユーザー情報を修正しました。";
 
-// 이슈
-JS_MESSAGE["issue.project.id.empty"] = "프로젝트를 선택하여 주십시오.";
-JS_MESSAGE["issue.issuetype.empty"] = "Issue Type을 선택하여 주십시오.";
-JS_MESSAGE["issue.datakey.empty"] = "Data Key를 입력하여 주십시오.";
-JS_MESSAGE["issue.title.empty"] = "제목을 입력하여 주십시오.";
-JS_MESSAGE["issue.assignee.empty"] = "대리자를 입력하여 주십시오";
-JS_MESSAGE["issue.reporter.empty"] = "보고 해야 하는 사람을 입력하여 주십시오.";
-JS_MESSAGE["issue.contents.empty"] = "내용을 입력하여 주십시오.";
-JS_MESSAGE["issue.due_day.invalid"] = "issue 마감일을 올바르게 입력하여 주십시오.";
-JS_MESSAGE["issue.due_time.invalid"] = "issue 마감시간을 올바르게 입력하여 주십시오.";
+//問題
+JS_MESSAGE["issue.project.id.empty"] = "プロジェクトを選択してください。";
+JS_MESSAGE["issue.issuetype.empty"] = "issueタイプを選択してください。";
+JS_MESSAGE["issue.datakey.empty"] = "日付キーを入力してください。";
+JS_MESSAGE["issue.title.empty"] = "タイトルを入力してください。";
+JS_MESSAGE["issue.assignee.empty"] = "代理者を入力してください";
+JS_MESSAGE["issue.reporter.empty"] = "報告者を入力してください。";
+JS_MESSAGE["issue.contents.empty"] = "内容を入力してください。";
+JS_MESSAGE["issue.due_day.invalid"] = "issue期限を正しく入力してください。";
+JS_MESSAGE["issue.due_time.invalid"] = "issue締切を正しく入力してください。";
