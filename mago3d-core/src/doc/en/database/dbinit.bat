6f06714abda150dea558f23eafe5d5b552417a99
Park Jeongyong <p1988@hanmir.com>
2018-04-12 23:51:16 +0900

    change from hardcoded path to variable in dbinit.bat

--- mago3d-core/src/doc/en/database/dbinit.bat
+++ mago3d-core/src/doc/en/database/dbinit.bat
@@ -3,37 +3,38 @@
 @echo off
 echo .................. ddl init start .................
 echo ...................................................
+SET CUR_PATH=%~dp0
 
 cd C:\PostgreSQL\9.6\bin\
 
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\access_log.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\api.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\common_code.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\datainfo.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\datainfo_log.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\file_info.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\issue.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\menu.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\policy.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\role.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\schedule.sql
-:: psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\sso_log.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\user_info.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\widget.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\access_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\api.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\common_code.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\datainfo.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\datainfo_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\file_info.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\issue.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\menu.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\policy.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\role.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\schedule.sql
+:: psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\sso_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\user_info.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\widget.sql
 
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\ddl\sequence\sequence.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%ddl\sequence\sequence.sql
 
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\index\access_log.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\index\api_log.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\index\data_info_log.sql
-:: psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\index\sso_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%index\access_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%index\api_log.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%index\data_info_log.sql
+:: psql -U postgres -d mago3d -a -f %CUR_PATH%index\sso_log.sql
 
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\trigger\access_log_trigger.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\trigger\api_log_trigger.sql
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\trigger\data_info_log_trigger.sql
-:: psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\trigger\sso_log_trigger.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%trigger\access_log_trigger.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%trigger\api_log_trigger.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%trigger\data_info_log_trigger.sql
+:: psql -U postgres -d mago3d -a -f %CUR_PATH%trigger\sso_log_trigger.sql
 
-psql -U postgres -d mago3d -a -f C:\git\repository\mago3d\mago3d-core\src\doc\en\database\dml\insert.sql
+psql -U postgres -d mago3d -a -f %CUR_PATH%dml\insert.sql
 
 echo .................. ddl init end ...................
 echo ...................................................
