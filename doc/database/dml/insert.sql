5eae55cc96908aa9a3d8772f8ae5fe9fcbea17a9
jdcheon <jdcheon@gaia3d.com>
2020-05-27 14:23:48 +0900

    insert 문 버그 수정

--- doc/database/dml/insert.sql
+++ doc/database/dml/insert.sql
@@ -10,7 +10,7 @@ insert into user_info(
 	user_id, user_group_id, user_name, password, user_role_check_yn, last_signin_date)
 values
 	('admin', 1, '슈퍼관리자', '비밀번호', 'N', now()),
-	('mago3d', 2, '스마트시티', '비밀번호', 'Y', now());
+	('mago3d', 2, 'Gaia3D', '비밀번호', 'Y', now());
 
 -- 관리자 메뉴
 insert into menu(menu_id, menu_type, menu_target, name, name_en, ancestor, parent, depth, view_order, url, url_alias, html_id, css_class, default_yn, use_yn, display_yn)
@@ -138,9 +138,9 @@ values
 	(NEXTVAL('user_group_role_seq'), 1, 3),
 	(NEXTVAL('user_group_role_seq'), 1, 4),
 	(NEXTVAL('user_group_role_seq'), 1, 5),
-	(NEXTVAL('user_group_role_seq'), 1, 7),
+	(NEXTVAL('user_group_role_seq'), 1, 6),
 	(NEXTVAL('user_group_role_seq'), 2, 4),
-	(NEXTVAL('user_group_role_seq'), 2, 5),
+	(NEXTVAL('user_group_role_seq'), 2, 5);
 
 
 -- 메인 화면 위젯
