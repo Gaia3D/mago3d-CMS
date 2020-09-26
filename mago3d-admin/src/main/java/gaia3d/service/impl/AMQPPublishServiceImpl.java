5fff606118e4026c9468f6a3a709511e80bfcd43
jdcheon <jdcheon@gaia3d.com>
2020-09-26 21:24:30 +0900

    queue send 수정

--- mago3d-admin/src/main/java/gaia3d/service/impl/AMQPPublishServiceImpl.java
+++ mago3d-admin/src/main/java/gaia3d/service/impl/AMQPPublishServiceImpl.java
@@ -27,6 +27,6 @@ public class AMQPPublishServiceImpl implements AMQPPublishService {
 	@Transactional
 	public void send(QueueMessage queueMessage) {
 		log.info("@@ Publish send message >>> {}", queueMessage);
-		rabbitTemplate.convertAndSend(propertiesConfig.getQueueName(), queueMessage);
+		rabbitTemplate.convertAndSend(propertiesConfig.getExchange(), propertiesConfig.getQueueName(), queueMessage);
 	}
 }
